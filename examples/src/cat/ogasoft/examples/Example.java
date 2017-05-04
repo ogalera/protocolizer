f734505b22832bd90fb7ff22ab5ade4011f101e4
oscar <oscar.galeraa@gmail.com>
2017-05-04 21:38:45 +0200

    Doc

--- examples/src/cat/ogasoft/examples/Example.java
+++ examples/src/cat/ogasoft/examples/Example.java
@@ -1,24 +1,31 @@
-# Protocolizer
-**Zero-overhead Java <-> Protocol Buffer** automatizer, at roughly 133Kb, the library is very light and only works in development pahse to get a single contact point with Protocol Buffer.
+/*
+ * Copyright 2017 Oscar Galera i Alfaro.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cat.ogasoft.examples;
 
--------------------------------------------------------------------
+import cat.ogasoft.protocolizer.annotations.ProtoFileV2;
+import java.util.List;
 
-## Description
-Write less and do more annotating POJO Java classes that represents a protocol buffer message and left Protocolizer works for you. It's very simple.
-
-### Structure
-![esquma](https://github.com/ogalera/protocolizer/blob/master/resources/esquema.png)
-
-Note that this library only works at compile time, so your project will never experiment any overhead ad deployment.
-
-![esquma](https://github.com/ogalera/protocolizer/blob/master/resources/intercanvi.png)
-
--------------------------------------------------------------------
-## Examples
-
-´´´Java
-@ProtoFileV2.File(pJavaName = "Example1")
-public class Example1 {
+/**
+ * @author Oscar Galera i Alfaro
+ * @date   May 4, 2017 [8:55:01 PM]
+ *
+ * @brief Very simple Protocolizer example.
+ */
+@ProtoFileV2.File(pJavaName = "Example")
+public class Example {
 
     @ProtoFileV2.File.Message
     public static class Person {
@@ -117,40 +124,3 @@ public class Example1 {
         }
     }
 }
-´´´
-
-Generated Google Protocol Buffer message
-´´´protoc
-option java_package = "cat.ogasoft.protocolizer.messages";
-
-option java_outer_classname = "Example1";
-
-
-message Person {
-    required string name = 1;
-    required int32 id = 2;
-    optional string email = 3;
-    required PhoneNumber phones = 4;
-    message PhoneNumber {
-        required string number = 1;
-        optional PhoneType type = 2;
-    }
-    enum PhoneType {
-        MOBILE = 0;
-        HOME = 1;
-        WORK = 2;
-    }
-}
-
-message AddressBook {
-    required Person people = 1;
-}
-´´´
-
--------------------------------------------------------------------
-## Installation
-You only need **four very simple steps**.
-1. Download **Google Protocol Buffer** and configure it in your IDE.
-2. Download **Protocolizer.jar** and add it at your class path.
-3. Configure the annotation processor (**cat.ogasoft.protocolizer.processor.ProtoFileProcessorV2**) in your IDE or classpath.
-4. Done.
